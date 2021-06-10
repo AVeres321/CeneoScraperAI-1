80bf15a17344e83f1531786cc3e00a9518762f62
nVeres <nastyaveres1@gmail.com>
2021-06-10 12:18:14 +0300

    2021-06-10 AV

--- app/forms.py
+++ app/forms.py
@@ -7,6 +7,6 @@ class ProductForm(FlaskForm):
         [
             validators.DataRequired(message = "Product Id must be given"),
             validators.Length(min=8, max=8, message= "Product Id must have 8 characters"),
-            validators.Regexp(regex="^[0-9]+$", message="Product Id can contain only digits")
+            validators.Regexp(regex="^[0-9]+$", message="Product Id can contain only numbers")
         ])
     submit = SubmitField('Extract')
--- app/models/product.py
+++ app/models/product.py
@@ -1,9 +1,10 @@
 from app import app
-from app.utils import extractElement
 from app.models.opinion import Opinion
+from app.utils import extractElement
+from bs4 import BeautifulSoup
+import pandas as pd
 import requests
 import json
-from bs4 import BeautifulSoup
 
 class Product:
 
@@ -25,7 +26,7 @@ class Product:
             pageDOM = BeautifulSoup(respons.text, 'html.parser')
             opinions = pageDOM.select("div.js_product-review")
             for opinion in opinions:
-                self.opinions.append(Opinion().extractOpinion(opinion))
+                self.opinions.append(Opinion().extractOpinion(opinion).transformOpinion())
             try:
                 url = self.url_pre + extractElement(pageDOM, 'a.pagination__next', "href") 
             except TypeError:
@@ -33,15 +34,30 @@ class Product:
 
     def exportProduct(self):
         with open("app/opinions/{}.json".format(self.productId), "w", encoding="UTF-8") as jf:
-            json.dump(self.__dict__(), jf, indent=4, ensure_ascii=False)
+            json.dump(self.toDict(), jf, indent=4, ensure_ascii=False)
+
+    def importProduct(self):
+        with open("app/opinions/{}.json".format(self.productId), "r", encoding="UTF-8") as jf:
+            product = json.load(jf)
+            self.name = product['name']
+            opinions = product['opinions']
+            for opinion in opinions:
+                self.opinions.append(Opinion(**opinion))
+        return self
 
     def __str__(self):
         return '''productId: {}<br>
         name: {}<br>'''.format(self.productId, self.name)+"<br>".join(str(opinion) for opinion in self.opinions)
 
-    def __dict__(self):
+    def toDict(self):
         return {
             "productId": self.productId,
             "name": self.name,
-            "opinions": [opinion.__dict__() for opinion in self.opinions]
+            "opinions": [opinion.toDict() for opinion in self.opinions]
         }
+
+    def opinionsToDataFrame(self):
+        opinions = pd.DataFrame.from_records(
+            [opinion.toDict() for opinion in self.opinions])
+        # opinions = pd.json_normalize([opinion.toDict() for opinion in self.opinions])
+        return opinions
